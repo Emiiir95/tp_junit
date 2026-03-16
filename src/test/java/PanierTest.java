17014c46d5899d8ebfa6ccf17ea02c1f1b1f9eff
Emir Sen <emirsen@OrdinatledeEmir.lan>
2026-03-16 11:42:51 +0100

    test panier

--- src/test/java/PanierTest.java
+++ src/test/java/PanierTest.java
@@ -7,9 +7,40 @@ class PanierTest {
         // Arranger
         Panier panier = new Panier();
         Article article = new Article("REF-001", "Stylo bleu", 1.50);
+
         // Agir
         panier.ajouterArticle(article, 2);
+
         // Affirmer
         assertEquals(1, panier.nombreArticles());
     }
+
+    @Test
+    void calculerTotalDoitRetournerLaSommeDessousTotaux() {
+        Panier panier = new Panier();
+        Article article1 = new Article("REF-001", "Stylo bleu", 1.50);
+        Article article2 = new Article("REF-002", "Stylo noir", 1.50);
+
+        panier.ajouterArticle(article1, 3);
+        panier.ajouterArticle(article2, 3);
+
+        assertEquals(9.00, panier.calculerTotal());
+    }
+
+    @Test
+    void panierVideDoitRetournerEstVideEgalTrue() {
+        Panier panier = new Panier();
+
+        assertTrue(panier.estVide());
+    }
+
+    @Test
+    void panierAvecArticlesNeDoitPasEtreVide() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo bleu", 1.50);
+        panier.ajouterArticle(article, 3);
+
+        assertFalse(panier.estVide());
+    }
+
 }
