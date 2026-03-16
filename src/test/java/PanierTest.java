e3328917b5b5d892b9aa60e699104dc7916853d2
Emir Sen <emirsen@OrdinatledeEmir.lan>
2026-03-16 13:32:45 +0100

    Cas invalides

--- src/test/java/PanierTest.java
+++ src/test/java/PanierTest.java
@@ -43,4 +43,49 @@ class PanierTest {
         assertFalse(panier.estVide());
     }
 
+    @Test
+    void quantiteNulleDoitLeverUneException() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo", 1.50);
+        assertThrows(IllegalArgumentException.class,
+                () -> panier.ajouterArticle(article, 0));
+    }
+
+    @Test
+    void articleNulDoitLeverException() {
+        Panier panier = new Panier();
+        assertThrows(IllegalArgumentException.class,
+                () -> panier.ajouterArticle(null, 1));
+    }
+
+    @Test
+    void quantiteNulleDoitLeverException() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo", 1.50);
+        assertThrows(IllegalArgumentException.class,
+                () -> panier.ajouterArticle(article, 0));
+    }
+
+    @Test
+    void quantiteNegativeDoitLeverException() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo", 1.50);
+        assertThrows(IllegalArgumentException.class,
+                () -> panier.ajouterArticle(article, -3));
+    }
+
+    @Test
+    void codeReductionVideDoitLeverException() {
+        Panier panier = new Panier();
+        assertThrows(IllegalArgumentException.class,
+                () -> panier.appliquerCodeReduction(""));
+    }
+
+    @Test
+    void codeReductionNulDoitLeverException() {
+        Panier panier = new Panier();
+        assertThrows(IllegalArgumentException.class,
+                () -> panier.appliquerCodeReduction(null));
+    }
+
 }
